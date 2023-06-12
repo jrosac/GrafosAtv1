48efcd66b8e72e9eb8f4f0e8ebb57a805e41c3c3
jrosac <131018470+jrosac@users.noreply.github.com>
2023-06-12 15:40:34 -0300

    Add files via upload
    Inicio do trabalho1 de grafos

---
+++ GrafosAtv/src/Matriz.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class Matriz {
+    private double[][] mat;
+    private int numerodelinhas;
+    private int numerodecolunas;
+
+    public Matriz(int nlinhas, int ncolunas) // cria uma matriz com todos os valores 0
+    {
+        numerodelinhas = nlinhas;
+        numerodecolunas = ncolunas;
+        mat = new double[numerodelinhas][numerodecolunas];
+
+        for(int i = 0; i < numerodelinhas; i++)
+        {
+            for(int j = 0; j < numerodecolunas ;j++)
+            {
+                mat[i][j] = 0;
+            }
+        }
+    }
+    //Cria uma matriz e pede
+    public Matriz()
+    {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println();
+        System.out.print("Digite o numero de linhas: "); numerodelinhas = teclado.nextInt();
+        System.out.print("Digite o numero de colunas: "); numerodecolunas = teclado.nextInt();
+        mat = new double[numerodelinhas][numerodecolunas];
+        for(int i = 0; i < numerodelinhas; i++)
+        {
+            for(int j = 0; j < numerodecolunas; j++)
+            {
+                System.out.print("Item ("+i+","+j+"): ");
+                mat[i][j] = teclado.nextDouble();
+            }
+        }
+    }
+    private void adicionarElemento(double elemento, int i, int j)
+    {
+        mat[i][j] = elemento;
+    }
+
+    public String toString()
+    {
+        String es = new String();
+
+        for(int i = 0; i < numerodelinhas; i++)
+        {
+            for(int j = 0; j < numerodecolunas; j++)
+            {
+                es += " "+ (int)mat[i][j];
+            }
+            es += '\n';
+        }
+        System.out.println();
+        System.out.println("sua matriz: ");
+        return es;
+    }
+
+}
+
+
+
---
+++ GrafosAtv/src/Principal.java
@@ -0,0 +1,9 @@
+public class Principal {
+    public static void main(String[] args) {
+
+        Matriz m01 = new Matriz();
+
+        System.out.print(m01.toString());
+
+    }
+}
